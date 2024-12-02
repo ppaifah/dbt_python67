a8e6b90a131540c9e60ee633682ead26a7b29dfb
ppaifah <mymelopapayaa@gmail.com>
2024-12-03 00:20:58 +0700

    ส่งงาน

---
+++ Assignment2_1.py
@@ -0,0 +1,27 @@
+import math
+a = 4
+b = 2
+c = 3 
+m = 2
+e = 3
+r = 4.5
+
+result1 = a * b
+result2 = a ** b
+result3 = a / b
+result4 = math.sqrt(a)
+result5 = math.sqrt((a**2)+(b**2))
+result6 = (m*c)**2
+result7 = 3/(4*math.pi*r**3)
+result8 = (a*b+c)/(b+1/e)
+
+print(result1)
+print(result2)
+print(result3)
+print(result4)
+print(result5)
+print(result6)
+print(result7)
+print(result8)
+
+
