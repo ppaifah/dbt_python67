80b2fc8605207e5d4acf03273dec7a5e10c39a64
ppaifahh <mymelopapayaa@gmail.com>
2024-12-10 20:14:45 +0700

    first commit

---
+++ Assignment1_2.py
@@ -0,0 +1,15 @@
+name = input("ระบุชื่อและนามสกุลของคุณ : ")
+major= input("ระบุสาขาของคุณ : ")
+years = input("ปีที่กำลังศึกษาอยู่ : ")
+like = input("สิ่งที่คุณชอบ : ")
+dislike = input("สิ่งที่คุณไม่ชอบ : ")
+
+print("="*35)
+print(f"ชื่อ - นามสกุล : {name}")
+print(f"สาขาที่กำลังศึกษา : {major}")
+print(f"ปีปัจจุบัน : {years}")
+print(f"สิ่งที่ชอบ : {like}")
+print(f"สิ่งที่ไม่ชอบ : {dislike}")
+print("="*35)
+
+
